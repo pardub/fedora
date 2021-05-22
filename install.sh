da1e23af0ee215ddb9c3b8d5ce7ac33ee27f7911
pardub <42610272+pardub@users.noreply.github.com>
2021-05-22 19:05:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo hostnamectl set-hostname fedora
 sudo dnf update -y
 sudo dnf -y install timeshift
 sudo dnf -y install vim
-sudo dnf -y install visudo
+#sudo dnf -y install visudo
 sudo dnf -y install git
 sudo dnf -y install gparted
 sudo dnf -y install util-linux-user
