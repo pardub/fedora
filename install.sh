e3a47f6a6287ec71ad6c3f88c9dcad9ca36b4cb2
pardub <42610272+pardub@users.noreply.github.com>
2021-05-22 22:57:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -148,5 +148,12 @@ sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker "$USER"
 
+### Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+### REBOOT
+reboot
+
 ## TEST WITHOUT SUDO
 docker run hello-world
