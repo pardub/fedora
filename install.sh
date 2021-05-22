956212be2b77fb400a5511f6c2e99fb561c87e76
pardub <42610272+pardub@users.noreply.github.com>
2021-05-22 21:07:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,3 +45,9 @@ sudo hostnamectl set-hostname fedora
 ### DISABLE SELINUX
 sudo sed -i 's/enforcing/disabled/' /etc/selinux/config
 
+### Download visual studio code
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf -y check-update
+sudo dnf -y  install code
