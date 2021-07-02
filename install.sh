16e452b4625f17e3b53d5523bfcd4a53cac87c7c
pardub <42610272+pardub@users.noreply.github.com>
2021-07-02 15:14:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,6 +61,9 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+###  ADD FLATPAK REPO
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 ### ADD GOOGLE DNS 
 echo 'nameserver=8.8.8.8' | sudo tee -a /etc/hosts
 
