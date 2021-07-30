9d7bde1832753c8274ae77d12d142a1d47da254f
pardub <42610272+pardub@users.noreply.github.com>
2021-07-30 20:04:45 +0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -42,6 +42,7 @@ sudo dnf -y install rclone
 sudo dnf -y install rclone-browser
 sudo dnf -y install audacity 3.0.2
 sudo dnf -y install dropbox
+sudo dnf -y chrome-remote-desktop
 
 sudo dnf -y install @virtualization
 # sudo dnf -y install clamav
