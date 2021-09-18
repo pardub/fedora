633d54293fd31c60ec650159c69e99f81b12f43f
pardub <42610272+pardub@users.noreply.github.com>
2021-09-18 19:34:36 +0300

    Update script2_global.sh

--- script2_global.sh
+++ script2_global.sh
@@ -5,8 +5,10 @@ sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-r
 sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 ### Fastest mirror and Delta RPM
-echo 'fastestmirror=true' | sudo tee -a /etc/dnf/dnf.conf
-echo 'deltarpm=true'      | sudo tee -a /etc/dnf/dnf.conf
+echo 'fastestmirror=True'             | sudo tee -a /etc/dnf/dnf.conf
+echo 'deltarpm=True'                  | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10'      | sudo tee -a /etc/dnf/dnf.conf
+sudo dnf clean all
 
 ###  ADD FLATPAK REPO
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
