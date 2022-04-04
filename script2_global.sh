974a9d5d22245d504cdadb3a7784f464ab28269c
pardub <42610272+pardub@users.noreply.github.com>
2022-04-04 16:55:47 +0300

    Update script2_global.sh

--- script2_global.sh
+++ script2_global.sh
@@ -159,8 +159,7 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize
 ### Download Visual Studio Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf -y check-update
-sudo dnf -y  install code
+sudo dnf -y check-update && sudo dnf -y  install code
 
 ### DOWNLOAD MESLO FONT
 
