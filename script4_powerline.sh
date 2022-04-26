f6b54f5a34104c22aa95573f6bb753e84a21163f
pardub <42610272+pardub@users.noreply.github.com>
2022-04-26 14:00:18 +0300

    Update script4_powerline.sh

--- script4_powerline.sh
+++ script4_powerline.sh
@@ -8,4 +8,6 @@ cd git/git_clone
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
+# auto-complete-clear
+rm ~/.zcompdump
 ### REBOOT
