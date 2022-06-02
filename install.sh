196efa29a674ebd8a2ef35f372e0bc0032831fb1
pardub <42610272+pardub@users.noreply.github.com>
2022-06-02 18:27:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -191,7 +191,8 @@ sudo usermod -aG docker "$USER"
 
 ### Docker Compose
 #sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-darwin-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 ### CHECK DOCKER COMPOSE VERSION
 docker-compose --version
